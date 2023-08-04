6c992bcc3fb7d72f70670dfcef4f4f5025d9ca87
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-08-04 10:28:19 +0200

    homepage with three js

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Welcome from '../../components/Welcome/Welcome'
 import './Homepage.css'
+import BasicScene from '../../components/3DObject/3DObject'
+import BasicAnimation from '../../components/BasicAnimation/BasicAnimation'
 
 function Homepage() {
   return (
     <div className='homepage-container'>
       <Welcome />
+      <br />
+      <BasicScene />
+      <BasicAnimation /> 
     </div>
   )
 }
